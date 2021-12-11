aad65aa795a6190f126c3c6694400073e56ad2a2
Jorge-Montoya <jorgemontoya192@gmail.com>
2021-12-11 13:21:39 -0600

    login y registro solo interfaz

---
+++ proyectov2/app/src/main/java/com/example/proyectov2/Login.java
@@ -0,0 +1,19 @@
+package com.example.proyectov2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
